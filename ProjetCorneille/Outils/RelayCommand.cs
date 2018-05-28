7d7f7f08e40c7a39c68ac642738d7bc1bcf46ff8
Yvann <yvann.josso@gmail.com>
2018-05-28 11:51:18 +0200

    Sprint 2 | Relay command pour MVVM

---
+++ ProjetCorneille/Outils/RelayCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProjetCorneille.Outils
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> executeAction;
+
+        /// <summary>
+        /// Le statut d'exécution.
+        /// </summary>
+        private Func<bool> canExecute;
+
+        /// <summary>
+        /// Commande d'exécution.
+        /// </summary>
+        /// <param name="executeAction">Délégué qui encapsule la méthode exécutée.</param>
+        public RelayCommand(Action<object> executeAction) : this(executeAction, null) { }
+
+        /// <summary>
+        /// Commande d'exécution.
+        /// </summary>
+        /// <param name="executeAction">Délégué qui encapsule la méthode exécutée.</param>
+        /// <param name="canExecute">Le statut d'exécution.</param>
+        public RelayCommand(Action<object> executeAction, Func<bool> canExecute)
+        {
+            if (executeAction == null) throw new ArgumentNullException("Aucune action fournie.");
+
+            this.executeAction = executeAction;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Statut d'exécution.
+        /// </summary>
+        /// <param name="parameter">Le paramètre d'exécution.</param>
+        /// <returns>Le statut d'exécution.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return (canExecute == null) ? true : canExecute.Invoke();
+        }
+
+        /// <summary>
+        /// Méthode d'exécution.
+        /// </summary>
+        /// <param name="parameter">Paramètre d'exécution.</param>
+        public void Execute(object parameter)
+        {
+            this.executeAction.Invoke(parameter);
+        }
+
+        /// <summary>
+        /// Évènement de changement de statut d'exécution.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- ProjetCorneille/ViewModel/MainPageViewModel.cs
+++ ProjetCorneille/ViewModel/MainPageViewModel.cs
@@ -1,17 +1,27 @@
-﻿using System;
+﻿using ProjetCorneille.Outils;
+using System;
 using System.ComponentModel;
+using System.Windows;
 
 namespace ProjetCorneille.ViewModel
 {
     public class MainPageViewModel : ViewModelBase
     {
         private string selectUserPath;
+        public RelayCommand CommandBouton { get; set; }
 
         public MainPageViewModel()
         {
-
+            CommandBouton = new RelayCommand(bonjour);
         }
 
+        private static void bonjour(object obj)
+        {
+            MessageBox.Show("Nique ta mère");
+        }
+
+
+      
         public string SelectUserPath
         {
             get
